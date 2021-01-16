06997d5a93126dc6f12cf76ba55c22c701470a0c
zhihanC <67808576+zhihanC@users.noreply.github.com>
2021-01-16 17:35:31 -0500

    Implemented basic CRUD functionality

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Allows Express to parse the body of post requests
+app.use(express.urlencoded({ extended: true }));
+//Allows npm package method-override to override default form reqs
+app.use(methodOverride('_method'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+//Get index of all campgrounds
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
+
+//Get form to make a new campground
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+//Post info from form to make a new campground
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//Get individual campground by id
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
+
+//Get form to update a campground
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+});
+
+//Put info from form to update a campground
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//Delete campground
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
+});
+
+// Server is listening
+app.listen(3000, () => {
+  console.log('Serving on port 3000');
+});
